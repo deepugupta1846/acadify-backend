790cb98b4e9ecda2a59dde5f7a009b9780933e0b
Deepu kumar <dk251248@gmail.com>
2026-06-14 18:12:19 +0000

    update port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const apiRoutes = require('./src/routes');
 const { handleLiveKitWebhook } = require('./src/@module/livekit/livekit.webhook');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 const isProduction = process.env.NODE_ENV === 'production';
 
 const corsOrigins = (process.env.CORS_ORIGIN || '')
